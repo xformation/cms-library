75e2d83c095ac2c168eb73f1ae2c384be44d96e8
TejaPrakashChoudary <38122603+tejaprakashchoudary@users.noreply.github.com>
2020-05-25 11:57:02 +0530

    Initial version of library generated by JHipster-6.7.1

---
+++ src/main/java/com/synectiks/library/config/Constants.java
@@ -0,0 +1,12 @@
+package com.synectiks.library.config;
+
+/**
+ * Application constants.
+ */
+public final class Constants {
+
+    public static final String SYSTEM_ACCOUNT = "system";
+
+    private Constants() {
+    }
+}
---
+++ src/main/java/com/synectiks/library/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.synectiks.library.config.audit;
